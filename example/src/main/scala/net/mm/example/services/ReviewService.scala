aad731ace6245123780fbb2aa43d5722e584b061
Moritz Mack <moritz@mackmail.net>
2015-01-30 09:52:15 +0100

    extended review model

--- example/src/main/scala/net/mm/example/services/ReviewService.scala
+++ example/src/main/scala/net/mm/example/services/ReviewService.scala
@@ -4,9 +4,9 @@ import net.mm.composer.relations.Relation._
 
 class ReviewService extends FakeService {
   private val reviews = Seq(
-    Review(1, 1, "steff"), Review(2, 1, "mark"), Review(3, 1, "chris"),
-    Review(4, 3, "mark"), Review(5, 3, "daniel"),
-    Review(6, 5, "steff"), Review(7, 5, "mark")
+    Review(1, 1, "steff", 4, "looks nice"), Review(2, 1, "mark", 3, "expensive"), Review(3, 1, "chris", 5, "awesome, always again"),
+    Review(4, 3, "mark", 3, "decent sound, quite ok"), Review(5, 3, "daniel", 3, "awesome price"),
+    Review(6, 5, "steff", 1, "bad quality"), Review(7, 5, "mark", 2, "works, but seen much better")
   ).map(r => (r.id, r)).toMap
 
   val getReviews: Executor[Int, Review] = reviews.filterKeys(_).asFuture
@@ -14,4 +14,4 @@ class ReviewService extends FakeService {
   val getReviewsByUser: Executor[String, Seq[Review]] = reviews.values.toSeq.groupBy(_.reviewerId).filterKeys(_).asFuture
 }
 
-case class Review(id: Int, productId: Int, reviewerId: String)
+case class Review(id: Int, productId: Int, reviewerId: String, stars: Int, review: String)
