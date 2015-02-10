0acc3b058a9ae4325e58ee68e7c88a809ff08624
Moritz Mack <moritz@mackmail.net>
2015-02-10 10:07:28 +0100

    minor simplifications for relation code

--- json-composer/src/main/scala/net/mm/composer/relations/Relation.scala
+++ json-composer/src/main/scala/net/mm/composer/relations/Relation.scala
@@ -4,10 +4,9 @@ import com.twitter.util.Future
 import net.mm.composer.relations.Relation._
 import net.mm.composer.relations.execution.{ExecutionHint, Hint, SerializationHint}
 
-sealed abstract class Relation[-From, Target, Key] {
+sealed abstract class Relation[-From, Target, Key](val target: Class[_]) {
   def key: RelationKey[From, Key]
   def apply: Executor[Key, Target]
-  def target: Class[_]
   def hints: Seq[Hint]
   val executionHints = hints.filter(_.isInstanceOf[ExecutionHint]).toSet
   val serializationHints = hints.filter(_.isInstanceOf[SerializationHint]).toSet
@@ -15,6 +14,9 @@ sealed abstract class Relation[-From, Target, Key] {
 
 object Relation {
   type AnyRelation = Relation[_, _, _]
+  type RelationFor[F] = Relation[F,_,_]
+  type RelationWithKey[K] = Relation[_,_,K]
+
   type AnyExecutor = Executor[_, _]
   type Executor[Key, Target] = Set[Key] => Future[Map[Key, Target]]
 
@@ -32,15 +34,7 @@ object Relation {
 }
 
 case class ToOne[-From, Target, Key](key: RelationKey[From, Key], apply: Executor[Key, Target], hints: Hint*)(implicit m: Manifest[Target])
-  extends Relation[From, Target, Key] {
+  extends Relation[From, Target, Key](m.runtimeClass)
 
-  def target = m.runtimeClass
-  //override def toString = s"ToOne(Key@${key.hashCode},Executor@${apply.hashCode})"
-}
-
-case class ToMany[-From, Target, Key](key: RelationKey[From, Key], apply: Executor[Key, Seq[Target]], hints: Hint*)(implicit m: Manifest[Seq[Target]])
-  extends Relation[From, Seq[Target], Key] {
-
-  def target = m.typeArguments.head.runtimeClass
-  //override def toString = s"ToMany(Key@${key.hashCode},Executor@${apply.hashCode})"
-}
+case class ToMany[-From, Target, Key](key: RelationKey[From, Key], apply: Executor[Key, Seq[Target]], hints: Hint*)(implicit m: Manifest[Target])
+  extends Relation[From, Seq[Target], Key](m.runtimeClass)
