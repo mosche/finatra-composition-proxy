219e7cd3a90091c7729c07c1a97af1a416e3237a
Moritz Mack <moritz@mackmail.net>
2015-04-24 12:59:39 +0200

    benchmark for properties validator

---
+++ benchmark/src/main/scala/net/mm/composer/properties/PropertiesValidationBenchmark.scala
@@ -0,0 +1,61 @@
+package net.mm.composer.properties
+
+import java.util.concurrent.TimeUnit
+
+import net.mm.composer.relations._
+import org.openjdk.jmh.annotations._
+
+@State(Scope.Benchmark)
+class PropertiesValidationBenchmark extends TestCases with TestCasesCompositionControllerBuilder{
+
+  val validator = new PropertiesValidatorImpl(classOf[Review])
+  val validProperties = Right(Seq(
+    FieldProperty("id"),
+    FieldProperty("productId"),
+    RelationProperty("product",
+      FieldProperty("id"),
+      RelationProperty("categories",
+        FieldProperty("id")
+      )
+    ),
+    FieldProperty("reviewerId"),
+    RelationProperty("reviewer",
+      FieldProperty("username"),
+      RelationProperty("myreviews",
+        FieldProperty("id"),
+        FieldProperty("productId"),
+        FieldProperty("reviewerId")
+      )
+    )
+  ))
+
+  val invalidProperties = Right(Seq(
+    FieldProperty("id"),
+    FieldProperty("productId"),
+    RelationProperty("product",
+      FieldProperty("id"),
+      RelationProperty("categories",
+        FieldProperty("id")
+      )
+    ),
+    FieldProperty("reviewerId"),
+    RelationProperty("reviewer",
+      FieldProperty("username"),
+      RelationProperty("invalid")
+    )
+  ))
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.AverageTime))
+  @OutputTimeUnit(TimeUnit.MILLISECONDS)
+  def validate_valid1: Unit = {
+    assert(validator.apply(validProperties).isRight)
+  }
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.AverageTime))
+  @OutputTimeUnit(TimeUnit.MILLISECONDS)
+  def validate_invalid1: Unit = {
+    assert(validator.apply(invalidProperties).isLeft)
+  }
+}
