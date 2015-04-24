b472ff14aabf5b5224a345a9b04d12b8064291b8
Moritz Mack <moritz@mackmail.net>
2015-04-24 13:01:31 +0200

    improved performance for properties validator

--- composition-proxy/src/main/scala/net/mm/composer/properties/PropertiesValidatorImpl.scala
+++ composition-proxy/src/main/scala/net/mm/composer/properties/PropertiesValidatorImpl.scala
@@ -3,36 +3,55 @@ package net.mm.composer.properties
 import net.mm.composer.relations.RelationRegistry
 import net.mm.composer.utils.SeqTypeFilterSupport._
 
+import scala.annotation.tailrec
+import scala.collection.mutable.Map
+
 /**
  * Validator to identify wrong properties that are neither
  * present as fields nor registered as relation.
  */
 class PropertiesValidatorImpl(clazz: Class[_])(implicit relationRegistry: RelationRegistry) extends PropertiesReader {
 
-  private def validate(properties: Seq[Property], clazz: Class[_]): Option[Error] = {
-    val clazzFields = clazz.getDeclaredFields.map(_.getName).toSet
+  @tailrec
+  private def validate(propsMap: Map[Class[_],Seq[Property]]): Option[Error] = {
 
-    val fields = properties.typeFilter[FieldProperty].map(_.name).toSet
-    val relations = properties.typeFilter[RelationProperty].map(_.name)
+    val propsMapIt = propsMap.iterator
+    val nextProps = Map.empty[Class[_], Seq[Property]]
 
-    val fieldsToCheck = fields -- clazzFields ++ relations
+    while(propsMapIt.hasNext){
+      val (clazz, props) = propsMapIt.next()
+      val (relations, fields) = props.typePartition[RelationProperty]
 
-    properties.filter(p => fieldsToCheck(p.name))
-      .map(p => (p, relationRegistry.get(clazz, p.name)))
-      .foldLeft[Option[Error]](None){
-        case (None, (property: FieldProperty, relation)) =>
-          relation.fold[Option[Error]](Some(s"Unknown field '${property.name}' for ${clazz.getSimpleName}"))(rel => None)
-        case (None, (property: RelationProperty, relation)) =>
-          relation.fold[Option[Error]](Some(s"Unknown relation '${property.name}' for ${clazz.getSimpleName}"))(rel =>
-            validate(property.properties, rel.target)
-          )
-        case (error, _) => error
+      val checkRequired = fields.map(_.name).toSet --
+        clazz.getDeclaredFields.toSeq.map(_.getName) ++
+        relations.map(_.name)
+
+      val propsIt = props.iterator
+      while(propsIt.hasNext){
+        val property = propsIt.next()
+
+        if(checkRequired(property.name)) {
+          val relation = relationRegistry.get(clazz, property.name)
+
+          if (relation.isEmpty)
+            return Some(s"Unknown ${property.description} for ${clazz.getSimpleName}")
+
+          property match {
+            case p: RelationProperty =>
+              val target = relation.get.target
+              nextProps += target -> nextProps.get(target).fold(p.properties)(_ ++ p.properties)
+            case _ =>
+          }
+        }
+      }
     }
+
+    if (nextProps.isEmpty) None else validate(nextProps)
   }
 
   override def apply(parsedProperties: Either[Error, Seq[Property]]): Either[Error, Seq[Property]] = {
     parsedProperties.right.flatMap{ properties =>
-      validate(properties, clazz).toLeft(properties)
+      validate(Map(clazz -> properties)).toLeft(properties)
     }
   }
 }
--- composition-proxy/src/main/scala/net/mm/composer/properties/Property.scala
+++ composition-proxy/src/main/scala/net/mm/composer/properties/Property.scala
@@ -2,12 +2,16 @@ package net.mm.composer.properties
 
 sealed trait Property {
   def name: String
+  def description: String
 }
 
-case class FieldProperty(name: String) extends Property
+case class FieldProperty(name: String) extends Property{
+  def description: String = s"field '$name'"
+}
 
 case class RelationProperty(name: String, modifiers: Map[String, Any], properties: Seq[Property]) extends Property {
   def childNames: Seq[String] = properties.map(_.name)
+  def description: String = s"relation '$name'"
 }
 
 object RelationProperty {
