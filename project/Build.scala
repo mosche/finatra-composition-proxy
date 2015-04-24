c0f82330a2c1e987cd5b7b8836bec83dd5267d52
Moritz Mack <moritz@mackmail.net>
2015-04-24 12:53:30 +0200

    added jmh benchmark module

--- project/Build.scala
+++ project/Build.scala
@@ -1,11 +1,12 @@
 import sbt.Defaults._
 import sbt.Keys._
 import sbt._
+import pl.project13.scala.sbt.SbtJmh.jmhSettings
 
 object Build extends Build{
 
   lazy val standardSettings = coreDefaultSettings ++ Seq(
-    scalaVersion := "2.10.4",
+    scalaVersion := "2.10.5",
     resolvers += "Twitter" at "http://maven.twttr.com",
     libraryDependencies ++= Seq(
       "com.twitter" %% "finatra" % "1.6.0",
@@ -16,6 +17,11 @@ object Build extends Build{
 
   lazy val `composition-proxy` = project
     .settings(standardSettings:_*)
+
+  lazy val `benchmark` = project
+    .settings(standardSettings:_*)
+    .settings(jmhSettings:_*)
+    .dependsOn(`composition-proxy` % "compile->compile;compile->test")
 
   lazy val example = project
     .settings(standardSettings:_*)
