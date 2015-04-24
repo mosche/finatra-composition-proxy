d61f5a5f8b008ed87158aa0496d3b9cfe8e3afce
Moritz Mack <moritz@mackmail.net>
2015-04-24 13:01:03 +0200

    new type partition operation for Seq

--- composition-proxy/src/main/scala/net/mm/composer/utils/SeqTypeFilterSupport.scala
+++ composition-proxy/src/main/scala/net/mm/composer/utils/SeqTypeFilterSupport.scala
@@ -3,6 +3,7 @@ package net.mm.composer.utils
 trait SeqTypeFilterSupport {
   implicit class TypeFilter[S](seq:Seq[S]){
     def typeFilter[T <: S](implicit m: Manifest[T]): Seq[T] = seq.filter(m.runtimeClass.isInstance).asInstanceOf[Seq[T]]
+    def typePartition[T <: S](implicit m: Manifest[T]): (Seq[T],Seq[S]) = seq.partition(m.runtimeClass.isInstance).asInstanceOf[(Seq[T],Seq[S])]
   }
 }
 
